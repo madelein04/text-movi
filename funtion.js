04f7d8b9be24bf9286f421fbb00ccb6bcb8f0d86
madelein04 <madepanqueva@gmail.com>
2023-01-16 18:17:19 -0500

    terminado

---
+++ funtion.js
@@ -0,0 +1,19 @@
+const textElem = document.getElementById("textico");
+const speedElem = document.getElementById("speed");
+
+const text = "We Love Programming!";
+let index = 1;
+let speed = 300 / speedElem.value;
+
+const writeText = () => {
+    textElem.innerText = text.slice(0, index);
+    index++;
+    if (index > text.length) {
+        index = 1;
+    }
+
+    setTimeout(writeText, speed);
+};
+
+writeText();
+speedElem.addEventListener("input", (e) => (speed = 300 / e.target.value));
